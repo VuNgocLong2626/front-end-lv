7521bfc8f444686db9f973621179aa2a76ff96c2
Vu_Long <long.speed00@gmail.com>
2022-09-22 14:06:46 +0700

    test

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <map-open-layers></map-open-layers>
 </template>
-
+ 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import MapOpenLayers from './components/MapOpenLayers.vue'
+ 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
+    MapOpenLayers
   }
 }
 </script>
-
+ 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/MapGoogle.vue
@@ -0,0 +1,21 @@
+<template>
+  <GoogleMap api-key="AIzaSyDHLHpUVR6850QajwNW4NjKJc9K4_QZQIg" style="width: 100%; height: 500px" :center="center" :zoom="16">
+    <Marker :options="{ position: center }" />
+  </GoogleMap>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+import { GoogleMap, Marker } from "vue3-google-map";
+
+export default defineComponent({
+  components: { GoogleMap, Marker },
+  setup() {
+    const center = { lat: 10.085329, lng: 105.748565 };
+
+    return { center };
+  },
+});
+</script>
+
+// AIzaSyBNHFac4c3LRNzl29s4RyFhKPCli3sfNDo
